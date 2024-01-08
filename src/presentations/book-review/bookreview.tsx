397943700ebdf3dd2f1d14c3f4adecd450f5e8a9
WorthyD <danieljworthy@gmail.com>
2024-01-08 06:44:21 -0600

    Updates

--- src/presentations/book-review/bookreview.tsx
+++ src/presentations/book-review/bookreview.tsx
@@ -10,3 +10,71 @@ const UILibraries = () => {
 };
 
 export default UILibraries;
+/*
+
+Find Opportunities
+Audbile
+1.75
+
+
+
+Essential Dev
+Code Complete
+
+Mythical Man Month
+
+Uncle Bob
+Clean Code
+Clean Coder
+Clean Architecture
+Clean Agile
+Clean Craftmanship
+
+Refactoring
+Pragmatic Programmer
+If Hemmingway Wrote Javascript
+
+Career Path:
+Tech
+The Staff Engineer's Path
+Staff Engineer
+
+
+Leadership
+Extreme Ownershi[p
+The manager's path
+Team Topologies
+Accelerate
+Team of Rivals -> e won because hepossessed an extraordinary ability to put himself in the place of other men, to experience what they were feeling, to understand their motives and desires.
+Leaders Eat Last
+
+
+Work In General
+The Goal
+Phoenix Project, Unicorn Project, DevOps handbook
+Team Geek
+The Inmates are Running the Asylum
+
+Life
+How to Win Friends & Influence People
+Carl newport -> Deep Work & Digital Minimalism
+Atomic Habits
+Never Split the Difference
+Boundaries 
+The Obstacle is the Wway
+7 Habits of Highly Effective People
+The Art of War
+Magic of Tidying Up/ Spark of Joy
+Build
+
+The fun Side
+Maniac Mansion
+master of doom adn doom guy
+Jason Schreier
+Humble Bundle
+
+
+
+
+
+*/
